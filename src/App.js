349fae87e79c42485c69f23e143594abc47335a4
DanielDonato <daniel_donato10@yahoo.com.br>
2019-10-19 21:56:30 -0300

    Criando pagina main e produtos, configurando rotas e pegar dados da api

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
+import Routes from './routes';
 import './styles.css';
 import Header from './components/Header/index';
+import Main from './pages/main';
+
 
 const App = () => (
   <div className="app">
     <Header/>
+    <Routes />
   </div>
 );
 
